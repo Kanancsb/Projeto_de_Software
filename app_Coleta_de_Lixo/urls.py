8985f9fcefe69b9c030435c4256964d79b0c2d4c
Kanan <kanancsb@gmail.com>
2024-04-15 17:58:00 -0300

    Adicionado novas views e sistema de autenticação

--- app_Coleta_de_Lixo/urls.py
+++ app_Coleta_de_Lixo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('Denuncias/Registro_Incidente/', views.registro_incidente, name='registro_incidente'),
     path('account/signup', views.signup, name='signup'),
     path('account/login', views.login_view, name='login'),
+    path('account/logout', views.logout_view, name='logout')
 ]
